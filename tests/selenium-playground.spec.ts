2abb5a04b4082041f36ba9f70dce50ba705ebd7c
Joshua Jangula <jjangula@pm.me>
2025-04-17 20:49:16 +0000

    Add exam tests.

---
+++ tests/selenium-playground.spec.ts
@@ -0,0 +1,174 @@
+import { test, expect } from '@playwright/test';
+import { TestBase } from './test-base';
+import { Log } from '../common/log';
+
+const testBase = new TestBase();
+const log = new Log();
+const baseUrl = 'https://www.lambdatest.com/selenium-playground/';
+
+test.beforeEach(async ({ page }) => {
+    log.Step(`Navigate to \'${baseUrl}\'.`);
+    await page.goto(baseUrl);
+});
+
+test.afterEach(async ({ browser }) => {
+    await testBase.Cleanup(browser);
+});
+
+test.describe('\'Simple Form Demo\' Page Tests', () => {
+
+    const hyperlinkText = 'Simple Form Demo';
+
+    test('Validate that the \'Your Message\' field displays the correct text.', async ({ page }) => {
+        // Arrange
+        const expectedUrl = 'simple-form-demo';
+        const messageText = 'Welcome to LambdaTest';
+
+        // Act
+        log.Step(`Click the \'${hyperlinkText}\' hyperlink.`);
+        const hyperlink = await page.getByRole('link', { name: hyperlinkText });
+        await hyperlink.click();
+
+        // Assert
+        log.Step(`Validate that the user is successfully navigated to the \'${expectedUrl}\' page.`);
+        const currentPageUrl = await page.url();
+        await expect(currentPageUrl).toContain(expectedUrl);
+
+        // Act
+        log.Step(`Enter \'${messageText}\' into the \'Enter Message\' text box.`);
+        const enterMessageTextbox = await page.locator('//input[@id=\'user-message\']');
+        await enterMessageTextbox.fill(messageText);
+
+        log.Step(`Click the \'Get Checked Value\' button.`);
+        const getCheckValueButton = await page.getByText('Get Checked Value', { exact: true});
+        await getCheckValueButton.click();
+
+        // Assert
+        log.Step(`Validate that the \'Your Message:\' field displays \'${messageText}\'.`);
+        const yourMessageFieldText = await page.locator('//p[@id=\'message\']').innerText();
+        await expect(yourMessageFieldText).toBe(messageText);
+    });
+});
+
+test.describe('\'Drag & Drop Sliders\' Page Tests', () => {
+
+    const hyperlinkText = 'Drag & Drop Sliders';
+
+    test('Validate that the slider value is successfully changed to value \'95\'.', async ({ page }) => {
+        // Arrange
+        const sliderDefaultValue = 15;
+        const expectedSliderValue = 95;
+
+        // Act
+        log.Step(`Click the \'${hyperlinkText}\' hyperlink.`);
+        const hyperlink = await page.getByRole('link', { name: hyperlinkText });
+        await hyperlink.click();
+
+        log.Step(`Change \'Default value ${sliderDefaultValue}\' slider value to \'${expectedSliderValue}\'.`);
+        const parent = await page.locator(`//div[contains(@id,'slider')][contains(.,\'Default value ${sliderDefaultValue}\')]`);
+        const slider = await parent.locator('//input[@type=\'range\']');
+        const box = await slider.boundingBox();
+        const xPos = await box.x;
+        const yPos = await box.y + box.height / 2;
+        await page.mouse.move(xPos, yPos);
+        await page.mouse.down();
+        await page.mouse.move(xPos + 465, yPos);
+        await page.mouse.up();
+
+        // Assert
+        log.Step(`Validate that the \'Default value ${sliderDefaultValue}\' slider box displays as \'${expectedSliderValue}\'.`);
+        const sliderValueBox = await parent.locator('//output[contains(@id,\'range\')]');
+        const sliderValueBoxText = await sliderValueBox.textContent();
+        await expect(sliderValueBoxText).toContain(expectedSliderValue.toString());
+    });
+});
+
+test.describe('\'Input Form Submit\' Page Tests', () => {
+
+    const hyperlinkText = 'Input Form Submit';
+
+    test('Validate that the \'Form Demo\' page is successfully submitted.', async ({ page }) => {
+        // Arrange
+        const name = 'John Doe';
+        const email = 'johndoe@email.com';
+        const password = 'Password123';
+        const company = 'Test Company, Inc.';
+        const website = 'https://www.google.com/';
+        const country = 'United States';
+        const city = 'Test City';
+        const address1 = '999 Validation Ave';
+        const address2 = 'Suite #100';
+        const state = 'Alabama';
+        const zipCode = '54321';
+        const expectedMessageText = 'Thanks for contacting us, we will get back to you shortly.';
+
+        // Act
+        log.Step(`Click the \'${hyperlinkText}\' hyperlink.`);
+        const hyperlink = await page.getByRole('link', { name: hyperlinkText });
+        await hyperlink.click();
+
+        log.Step('Click the \'Submit\' button.');
+        const submitButton = await page.getByRole('button', { name: 'Submit' })
+        await submitButton.click();
+
+        log.Step('Validate that the \'Please fill out this field\' error message is displayed for the \'Name\' field.');
+        const nameTextbox = await page.getByPlaceholder('Name', { exact: true });
+        const textboxAlert = await nameTextbox.evaluate((inputElement) => {
+            const input = inputElement as HTMLInputElement;
+            input.reportValidity();
+            return input.validationMessage;
+        });
+        await expect(textboxAlert).toBe('Please fill out this field.');
+
+        log.Step('Enter value into the \'Name\' textbox.')
+        await nameTextbox.fill(name);
+
+        log.Step('Enter value into the \'Email\' textbox.')
+        const emailTextbox = await page.getByPlaceholder('Email', { exact: true })
+        await emailTextbox.fill(email);
+
+        log.Step('Enter value into the \'Password\' textbox.')
+        const passwordTextbox = await page.getByPlaceholder('Password', { exact: true });
+        await passwordTextbox.fill(password);
+
+        log.Step('Enter value into the \'Company\' textbox.')
+        const companyTexbox = await page.getByPlaceholder('Company', { exact: true });
+        await companyTexbox.fill(company);
+
+        log.Step('Enter value into the \'Website\' textbox.')
+        const websiteTextbox = await page.getByPlaceholder('Website', { exact: true });
+        await websiteTextbox.fill(website);
+
+        log.Step('Enter value into the \'Country\' textbox.')
+        const countryCombobox = await page.getByRole('combobox');
+        await countryCombobox.selectOption(country);
+
+        log.Step('Enter value into the \'City\' textbox.')
+        const cityTextbox = await page.getByPlaceholder('City', { exact: true });
+        await cityTextbox.fill(city);
+
+        log.Step('Enter value into the \'Address 1\' textbox.')
+        const address1Textbox = await page.getByPlaceholder('Address 1', { exact: true });
+        await address1Textbox.fill(address1);
+
+        log.Step('Enter value into the \'Address 2\' textbox.')
+        const address2Textbox = await page.getByPlaceholder('Address 2', { exact: true });
+        await address2Textbox.fill(address2);
+
+        log.Step('Enter value into the \'State\' textbox.')
+        const stateTextbox = await page.getByPlaceholder('State', { exact: true });
+        await stateTextbox.fill(address2);
+
+        log.Step('Enter value into the \'Zip Code\' textbox.')
+        const zipCodeTextbox = await page.getByPlaceholder('State', { exact: true });
+        await zipCodeTextbox.fill(zipCode);
+
+        log.Step('Click the \'Submit\' button.');
+        await submitButton.click();
+
+        // Assert
+        log.Step(`Validate that the \'${expectedMessageText}\' message is successfully displayed.`);
+        const actualMessageText = await page.locator('//p[contains(@class,\'success-msg\')]').innerText();
+        await expect(actualMessageText).toBe(expectedMessageText);
+    });
+});
---
+++ tests/test-base.ts
@@ -0,0 +1,25 @@
+import { Browser, test } from '@playwright/test';
+import { Log } from '../common/log';
+
+const log = new Log();
+
+export class TestBase {
+    async Cleanup(browser: Browser) {
+        const contexts = browser.contexts();
+
+        log.Step("Close browser.");
+        for (const context of contexts) {
+            await context.close();
+        }
+    }
+}
+
+test.beforeAll(async () => {
+    log.HeaderText();
+});
+
+test.afterAll(async () => {
+    log.FooterText();
+});
+
+export default test;
