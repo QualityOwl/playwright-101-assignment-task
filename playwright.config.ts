1811c2d8d7abf8cc4399d73cff2d4815fbd35a65
QualityOwl <josh@qualityowl.llc>
2025-04-22 15:30:42 -0500

    Update package versions.

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,28 +5,41 @@ dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
-  /* Do not run tests in parallel */
+
+  // Run tests in parallel
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+
+  // Fail the build on CI if you accidentally left test.only in the source code.  
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+
+  // Retry failed tests two times
+  retries: 2,
+
+  // Fail if a test passes after retry
+  failOnFlakyTests: true,
+
+  // Opt out of parallel tests on CI.
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
+  // Reporter to use. See https://playwright.dev/docs/test-reporters
   reporter: process.env.CI
-    ? [['github'], ['html', { outputFolder: 'playwright-report', open: 'never' }], ['junit', { outputFile: 'results/test-results.xml' }]]
+    ? [
+      ['github'],
+      ['html', { outputFolder: 'playwright-report', open: 'never' }],
+      ['junit', { outputFile: 'results/test-results.xml' }]
+    ]
     : 'list',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  // Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
+    // Base URL to use in actions like `await page.goto('/')`.
     // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
+    trace: 'retain-on-failure',
   },
 
-  /* Configure projects for major browsers */
+  // Configure projects for major browsers
   projects: [
     {
       name: 'chromium',
